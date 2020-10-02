066d82822511baf951a95f4bca1b54e90543af46
Gnana Prakash Reddy <gnanaprakash2505@gmail.com>
2020-10-02 15:02:17 +0530

    Adding switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React,{Fragment, useEffect} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
 import {loadUser} from './actions/auth';
@@ -28,8 +28,10 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar/>
-          <Route exact path='/' component={Landing}/>
-          <Route component={Routes} />
+          <Switch>
+            <Route exact path='/' component={Landing}/>
+            <Route component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
